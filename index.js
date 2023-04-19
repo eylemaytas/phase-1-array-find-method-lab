177cd9c525bf58ae164a5204a187a9c223f2102b
Eylem Aytas <eylem.aytas@hotmail.com>
2023-04-19 00:11:12 -0400

    Modifies some files

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function superbowlWin(array) {
+    const win = array.find((game) => game.result === "W");
+    if (win) {
+      return win.year;
+    }
+  }
+  
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+  //...
+];
+
+const winYear = superbowlWin(record);
+console.log(winYear);
